9c35524081dda40a9809f31aa8e809ea174e3263
jefff341 <mynamesjefff34@gmail.com>
2024-12-15 15:31:43 -0500

    Initial commit

---
+++ src/main/java/vance/profit/Guaranteed_profit.java
@@ -0,0 +1,16 @@
+package vance.profit;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Guaranteed_profit implements ModInitializer {
+	public static final String MOD_ID = "guaranteed_profit";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
