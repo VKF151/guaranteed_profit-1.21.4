a5d12a32f0e5ef934d719ce70294e1bb2d419f12
jefff341 <mynamesjefff34@gmail.com>
2024-12-16 02:41:25 -0500

    2

---
+++ src/main/java/vance/profit/block/ModBlocks.java
@@ -0,0 +1,34 @@
+package vance.profit.block;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+import vance.profit.Guaranteed_profit;
+
+public class ModBlocks {
+    public static Block register(Block block, String name, boolean shouldRegisterItem) {
+        Identifier id = Identifier.of(Guaranteed_profit.MOD_ID,  name);
+
+        if (shouldRegisterItem) {
+            BlockItem blockItem = new BlockItem(block, new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM,Identifier.of(Guaranteed_profit.MOD_ID, name))));
+            Registry.register(Registries.ITEM, id, blockItem);
+        }
+
+        return Registry.register(Registries.BLOCK, id, block);
+    }
+
+    public static final Block SUPER_DIAMOND_BLOCK = register(
+            new Block(AbstractBlock.Settings.create().registryKey(RegistryKey.of(RegistryKeys.BLOCK,Identifier.of(Guaranteed_profit.MOD_ID, "super_diamond_block"))).sounds(BlockSoundGroup.METAL)),
+            "super_diamond_block",
+            true
+    );
+
+    public  static void initialize() {}
+}
--- src/main/java/vance/profit/item/ModItems.java
+++ src/main/java/vance/profit/item/ModItems.java
@@ -1,14 +1,18 @@
 package vance.profit.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import vance.profit.Guaranteed_profit;
+import vance.profit.block.ModBlocks;
 
 public class ModItems {
     public static final Item SUPER_DIAMOND = registerItem("super_diamond", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM,Identifier.of(Guaranteed_profit.MOD_ID, "super_diamond")))));
@@ -17,11 +21,20 @@ public class ModItems {
         return Registry.register(Registries.ITEM, Identifier.of(Guaranteed_profit.MOD_ID, name), item);
     }
 
+    public static final RegistryKey<ItemGroup> GUARANTEED_PROFIT_GROUP_KEY = RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of(Guaranteed_profit.MOD_ID, "guaranteed_profit_group"));
+    public static final ItemGroup GUARANTEED_PROFIT_GROUP = FabricItemGroup.builder()
+            .icon(() -> new ItemStack(ModItems.SUPER_DIAMOND))
+            .displayName(Text.translatable("itemGroup.guaranteed_profit"))
+            .build();
+
     public static void registerModItems() {
         Guaranteed_profit.LOGGER.info("Registering Mod Items for " + Guaranteed_profit.MOD_ID);
 
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+        Registry.register(Registries.ITEM_GROUP, GUARANTEED_PROFIT_GROUP_KEY, GUARANTEED_PROFIT_GROUP);
+
+        ItemGroupEvents.modifyEntriesEvent(GUARANTEED_PROFIT_GROUP_KEY).register(fabricItemGroupEntries -> {
             fabricItemGroupEntries.add(SUPER_DIAMOND);
+            fabricItemGroupEntries.add(ModBlocks.SUPER_DIAMOND_BLOCK.asItem());
         });
     }
 }
